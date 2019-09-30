057bce9fb8b63e0362972e5a68a1ffe87b3275b8
chairmanPanda <lusi@tcd.ie>
2019-09-30 19:07:48 +0100

    Exercise 1 done

--- assessment/assessment.pl
+++ assessment/assessment.pl
@@ -9,11 +9,12 @@ add(succ(X),Y,succ(Z)) :- add(X,Y,Z).
 
 %Exercise 1
 
-add2(0,X+Y,Z) :- add(X,Y,Z).
-add2(X+Y,0,Z) :- add(X,Y,Z).
-
-add2(X+Y,succ(A+B),Z) :- add(X,Y,W), add(succ(A),B,C),add2(W,C,Z).
+add2(0,X+Y,Z) :- add2(X,Y,Z).
+add2(X+Y,0,Z) :- add2(X,Y,Z).
+add2(X+Y,succ(A+B),Z) :- add(X,Y,W), add2(succ(0),A+B,C),add2(W,C,Z).
+add2(succ(X+Y),A+B,Z) :- add2(succ(0),X+Y,W), add(A,B,C),add2(W,C,Z).
+%add2(X+Y,succ(A+B),Z) :- add(X,Y,W), add(A,B,C),add(succ(0),C,D),add(W,D,Z).
 add2(X+Y,R,Z) :- add(X,Y,A), add(A,R,Z).
 add2(X,Y+R,Z) :- add(Y,R,A), add(X,A,Z).
-%add2(X+Y,A+B,Z) :- add(X,Y,W), add(A,B,C) ,add2(W,C,Z).
+add2(X+Y,A+B,Z) :- add(X,Y,W), add(A,B,C) ,add(W,C,Z).
 add2(X,Y,Z) :- add(X,Y,Z).
