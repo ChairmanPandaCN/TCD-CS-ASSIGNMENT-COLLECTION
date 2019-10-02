7214657f344201a00be7579c03224b56aebf9fb3
SiKai <lusi@tcd.ie>
2019-10-02 12:38:39 +0100

    simple test passes,but not for complicated

--- assessment/assessment.pl
+++ assessment/assessment.pl
@@ -6,21 +6,22 @@ numeral(X+Y) :- numeral(X), numeral(Y).
 
 add(0,X,X).
 add(p(X),s(X),0).
-add(p(X),s(Y),Z) :- add(X,Y,Z). 
-add(s(X),p(Y),Z) :- add(X,Y,Z). 
 add(s(X),Y,s(Z)) :- add(X,Y,Z).
 add(p(X),Y,p(Z)) :- add(X,Y,Z).
 
 
-
-add2(p(X),Y,Z) :- add2(X,p(0),W),add2(W,Y,Z).
-add2(s(X),Y,Z) :- add2(X,s(0),W),add2(W,Y,Z).
-add2(X,p(Y),Z) :- add2(p(0),Y,W),add2(X,W,Z).
-add2(X,s(Y),Z) :- add2(s(0),Y,W),add2(X,W,Z).
-
-
 add2(0,X+Y,Z) :- add2(X,Y,Z).
 add2(X+Y,0,Z) :- add2(X,Y,Z).
+
+add2(s(p(X)),Y,Z):- add2(X,Y,Z).
+add2(p(s(X)),Y,Z):- add2(X,Y,Z).
+add2(X,s(p(Y)),Z):- add2(X,Y,Z).
+add2(X,p(s(Y)),Z):- add2(X,Y,Z).
+add2(p(s(X)),s(Y),Z) :- add2(s(X),Y,Z).
+add2(s(p(X)),p(Y),Z) :- add2(p(X),Y,Z).
+add2(s(X),p(s(Y)),Z) :- add2(X,s(Y),Z).
+add2(p(X),s(p(Y)),Z) :- add2(X,p(Y),Z).
+
 
 add2(X+Y,s(A+B),Z) :- add2(X,Y,W), add2(s(0),A+B,C),add2(W,C,Z).
 add2(s(X+Y),A+B,Z) :- add2(s(0),X+Y,W), add2(A,B,C),add2(W,C,Z).
@@ -28,11 +29,9 @@ add2(X,s(A+B),Z) :- add2(s(0),A+B,C),add2(X,C,Z).
 add2(s(X+Y),A,Z) :- add2(s(0),X+Y,W),add2(W,A,Z).
 add2(s(X+Y),s(A+B),Z) :- add2(s(0),X+Y,W),add2(s(0),A+B,C),add2(W,C,Z).
 
-add2(X+Y,p(A+B),Z) :- add2(X,Y,W), add2(p(0),A+B,C),add2(W,C,Z).
-add2(p(X+Y),A+B,Z) :- add2(p(0),X+Y,W), add2(A,B,C),add2(W,C,Z).
-add2(X,p(A+B),Z) :- add2(p(0),A+B,C),add2(X,C,Z).
-add2(p(X+Y),A,Z) :- add2(p(0),X+Y,W),add2(W,A,Z).
-add2(p(X+Y),p(A+B),Z) :- add2(p(0),X+Y,W),add2(p(0),A+B,C),add2(W,C,Z).
+
+
+
 
 add2(X+Y,R,Z) :- add2(X,Y,A), add2(A,R,Z).
 add2(X,Y+R,Z) :- add2(Y,R,A), add2(X,A,Z).
@@ -40,12 +39,10 @@ add2(X+Y,A+B,Z) :- add2(X,Y,W), add2(A,B,C) ,add2(W,C,Z).
 add2(X,Y,Z) :- add(X,Y,Z).
 
 
-
 /* 
     purly for fun.=.=
-
 Exercise 1
   test case 
-    1.add2(s(0+s(0+s(0))))+s(0)+s(0),s(0+s(0+s(0)))),Z).
+    1.add2(s(0+s(0+s(0)))+s(0)+s(0),s(0+s(0+s(0))),Z).
     2.add2(s(0)+s(0)+s(0)+s(0)+s(0),0,Z).
 */
--- assessment/tmp.pl
+++ assessment/tmp.pl
@@ -8,21 +8,26 @@ add(0,X,X).
 add(p(X),s(X),0).
 add(s(X),Y,s(Z)) :- add(X,Y,Z).
 add(p(X),Y,p(Z)) :- add(X,Y,Z).
-%Exercise 1
-/*
 
-*/
 add2(0,X+Y,Z) :- add2(X,Y,Z).
 add2(X+Y,0,Z) :- add2(X,Y,Z).
+
 add2(X+Y,s(A+B),Z) :- add2(X,Y,W), add2(s(0),A+B,C),add2(W,C,Z).
 add2(s(X+Y),A+B,Z) :- add2(s(0),X+Y,W), add2(A,B,C),add2(W,C,Z).
-
 add2(X,s(A+B),Z) :- add2(s(0),A+B,C),add2(X,C,Z).
 add2(s(X+Y),A,Z) :- add2(s(0),X+Y,W),add2(W,A,Z).
-
 add2(s(X+Y),s(A+B),Z) :- add2(s(0),X+Y,W),add2(s(0),A+B,C),add2(W,C,Z).
 
 add2(X+Y,R,Z) :- add2(X,Y,A), add2(A,R,Z).
 add2(X,Y+R,Z) :- add2(Y,R,A), add2(X,A,Z).
 add2(X+Y,A+B,Z) :- add2(X,Y,W), add2(A,B,C) ,add2(W,C,Z).
 add2(X,Y,Z) :- add(X,Y,Z).
+
+
+/* 
+    purly for fun.=.=
+Exercise 1
+  test case 
+    1.add2(s(0+s(0+s(0)))+s(0)+s(0),s(0+s(0+s(0))),Z).
+    2.add2(s(0)+s(0)+s(0)+s(0)+s(0),0,Z).
+*/
