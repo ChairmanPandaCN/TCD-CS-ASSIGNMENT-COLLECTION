14263b2c8e1412ab09731afd06bdbb6c04853b45
SiKai <lusi@tcd.ie>
2019-10-03 15:54:44 +0100

    starting writing exercise4

--- assessment/assessment.pl
+++ assessment/assessment.pl
@@ -1,6 +1,7 @@
 numeral(0).
 numeral(s(X)) :- numeral(X).
 numeral(p(X)) :- numeral(X).
+numeral(-X) :- numeral(X).
 numeral(X+Y) :- numeral(X), numeral(Y).
 
 
@@ -8,30 +9,48 @@ add(0,X,X).
 add(s(X),Y,s(Z)) :- add(X,Y,Z).
 add(p(X),Y,p(Z)) :- add(X,Y,Z).
 
-
-add2(0,X+Y,Z) :- add2(X,Y,Z).
-add2(X+Y,0,Z) :- add2(X,Y,Z).
-
+% non minus sign
+add2(0,X+Y,Z) :- expand(X,0,A),expand(Y,0,B),add2(A,B,Z).
+add2(X+Y,0,Z) :- expand(X,0,A),expand(Y,0,B),add2(A,B,Z).
 add2(X+Y,s(A+B),Z) :- add2(X,Y,W), add2(s(0),A+B,C),add2(W,C,Z).
 add2(s(X+Y),A+B,Z) :- add2(s(0),X+Y,W), add2(A,B,C),add2(W,C,Z).
 add2(X,s(A+B),Z) :- add2(s(0),A+B,C),add2(X,C,Z).
 add2(s(X+Y),A,Z) :- add2(s(0),X+Y,W),add2(W,A,Z).
 add2(s(X+Y),s(A+B),Z) :- add2(s(0),X+Y,W),add2(s(0),A+B,C),add2(W,C,Z).
-
-
 add2(X+Y,p(A+B),Z) :- add2(X,Y,W), add2(p(0),A+B,C),add2(W,C,Z).
 add2(p(X+Y),A+B,Z) :- add2(p(0),X+Y,W), add2(A,B,C),add2(W,C,Z).
 add2(X,p(A+B),Z) :- add2(p(0),A+B,C),add2(X,C,Z).
 add2(p(X+Y),A,Z) :- add2(p(0),X+Y,W),add2(W,A,Z).
 add2(p(X+Y),p(A+B),Z) :- add2(p(0),X+Y,W),add2(p(0),A+B,C),add2(W,C,Z).
-
 add2(X+Y,R,Z) :- expand(X,0,A),expand(Y,0,B),expand(R,0,C),add2(A,B,D),add2(D,C,Z).
-
 add2(X,Y+R,Z) :- expand(X,0,A),expand(Y,0,B),expand(R,0,C),add2(A,B,D),add2(D,C,Z).
-
 add2(X+Y,A+B,Z) :- expand(X,0,D),expand(Y,0,E),expand(A,0,F),expand(B,0,G),add2(D,E,H),add2(F,G,I),add2(H,I,Z).
-add2(X,Y,Z) :- add(X,Y,Z).
 
+
+%minus sign
+add2((-X),Y,Z) :- minus(X,A),expand(A,0,B),expand(Y,0,C),add2(B,C,Z).
+add2(X,(-Y),Z) :- minus(Y,A),expand(A,0,B),expand(X,0,C),add2(B,C,Z).
+add2(0,(-X)+Y,Z) :- minus(X,A),expand(A,0,B),expand(Y,0,C),add2(B,C,Z).
+add2(0,X+(-Y),Z) :- minus(Y,A),expand(A,0,B),expand(X,0,C),add2(B,C,Z).
+add2((-X)+Y,0,Z) :- minus(X,A),expand(A,0,B),expand(Y,0,C),add2(B,C,Z).
+add2(X+(-Y),0,Z) :- minus(Y,A),expand(A,0,B),expand(X,0,C),add2(B,C,Z).
+%add2(X+Y,s(A+B),Z) :- add2(X,Y,W), add2(s(0),A+B,C),add2(W,C,Z).
+%add2(s(X+Y),A+B,Z) :- add2(s(0),X+Y,W), add2(A,B,C),add2(W,C,Z).
+%add2(X,s(A+B),Z) :- add2(s(0),A+B,C),add2(X,C,Z).
+%add2(s(X+Y),A,Z) :- add2(s(0),X+Y,W),add2(W,A,Z).
+%add2(s(X+Y),s(A+B),Z) :- add2(s(0),X+Y,W),add2(s(0),A+B,C),add2(W,C,Z).
+%add2(X+Y,p(A+B),Z) :- add2(X,Y,W), add2(p(0),A+B,C),add2(W,C,Z).
+%add2(p(X+Y),A+B,Z) :- add2(p(0),X+Y,W), add2(A,B,C),add2(W,C,Z).
+%add2(X,p(A+B),Z) :- add2(p(0),A+B,C),add2(X,C,Z).
+%add2(p(X+Y),A,Z) :- add2(p(0),X+Y,W),add2(W,A,Z).
+%add2(p(X+Y),p(A+B),Z) :- add2(p(0),X+Y,W),add2(p(0),A+B,C),add2(W,C,Z).
+%add2(X+Y,R,Z) :- expand(X,0,A),expand(Y,0,B),expand(R,0,C),add2(A,B,D),add2(D,C,Z).
+%add2(X,Y+R,Z) :- expand(X,0,A),expand(Y,0,B),expand(R,0,C),add2(A,B,D),add2(D,C,Z).
+%add2(X+Y,A+B,Z) :- expand(X,0,D),expand(Y,0,E),expand(A,0,F),expand(B,0,G),add2(D,E,H),add2(F,G,I),add2(H,I,Z).
+
+
+
+add2(X,Y,Z) :- add(X,Y,Z).
 
 /*
 main purpse of this block is to eliminate redundant expression such as reduce p(p(p(s(s(0))))) to p(0).
@@ -42,7 +61,7 @@ expand(p(X),s(Y),Z) :- expand(X,Y,Z).
 expand(s(X),Y,Z) :- add2(s(0),Y,W),expand(X,W,Z).
 expand(p(X),Y,Z) :- add2(p(0),Y,W),expand(X,W,Z).
 expand(X+Y,W,Z) :- expand(X,W,R),expand(Y,R,Z). 
-
+expand(-X,W,Z) :- minus(X,Y),expand(Y,W,Z).
 
 /*
 main purpse of this block is to find opposite expression for the given parameter 
@@ -65,6 +84,6 @@ Exercise 2
     2.add2(s(0+p(0+s(0)))+s(0)+s(0),s(0+p(0+s(0))),Z).
 Exercise 3
   test case 
-    1.minus(s(0+p(0+s(0)))+s(0)+s(0)+s(0+p(0+s(0))))
+    1.minus(s(0+p(0+s(0)))+s(0)+s(0)+s(0+p(0+s(0))),Z).
 */
 
