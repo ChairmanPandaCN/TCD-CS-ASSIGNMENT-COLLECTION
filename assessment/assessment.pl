747d5e4224795348df573105c503a9e260694585
SiKai <lusi@tcd.ie>
2019-10-03 12:47:16 +0100

    restore deleted branch exercise2

--- assessment/assessment.pl
+++ assessment/assessment.pl
@@ -17,8 +17,6 @@ add2(s(X+Y),A+B,Z) :- add2(s(0),X+Y,W), add2(A,B,C),add2(W,C,Z).
 add2(X,s(A+B),Z) :- add2(s(0),A+B,C),add2(X,C,Z).
 add2(s(X+Y),A,Z) :- add2(s(0),X+Y,W),add2(W,A,Z).
 add2(s(X+Y),s(A+B),Z) :- add2(s(0),X+Y,W),add2(s(0),A+B,C),add2(W,C,Z).
-
-
 add2(X+Y,p(A+B),Z) :- add2(X,Y,W), add2(p(0),A+B,C),add2(W,C,Z).
 add2(p(X+Y),A+B,Z) :- add2(p(0),X+Y,W), add2(A,B,C),add2(W,C,Z).
 add2(X,p(A+B),Z) :- add2(p(0),A+B,C),add2(X,C,Z).
@@ -26,9 +24,7 @@ add2(p(X+Y),A,Z) :- add2(p(0),X+Y,W),add2(W,A,Z).
 add2(p(X+Y),p(A+B),Z) :- add2(p(0),X+Y,W),add2(p(0),A+B,C),add2(W,C,Z).
 
 add2(X+Y,R,Z) :- expand(X,0,A),expand(Y,0,B),expand(R,0,C),add2(A,B,D),add2(D,C,Z).
-
 add2(X,Y+R,Z) :- expand(X,0,A),expand(Y,0,B),expand(R,0,C),add2(A,B,D),add2(D,C,Z).
-
 add2(X+Y,A+B,Z) :- expand(X,0,D),expand(Y,0,E),expand(A,0,F),expand(B,0,G),add2(D,E,H),add2(F,G,I),add2(H,I,Z).
 add2(X,Y,Z) :- add(X,Y,Z).
 
@@ -40,7 +36,8 @@ expand(s(X),Y,Z) :- add2(s(0),Y,W),expand(X,W,Z).
 expand(p(X),Y,Z) :- add2(p(0),Y,W),expand(X,W,Z).
 expand(X+Y,W,Z) :- expand(X,W,R),expand(Y,R,Z). 
 
-
+minus(0,0).
+minus(X,Y):-expand(X,0,Y).
 /* 
     purly for fun.=.=
 Exercise 1
