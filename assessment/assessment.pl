81ab92a946f79b769e9993991fa56ff52af69ad3
SiKai <lusi@tcd.ie>
2019-10-05 11:25:14 +0100

    exercise 4 done

--- assessment/assessment.pl
+++ assessment/assessment.pl
@@ -6,28 +6,33 @@ numeral(X+Y) :- numeral(X), numeral(Y).
 
 
 add(0,X,X).
+add(s(X),p(Y),Z) :- add(X,Y,Z).
+add(p(X),s(Y),Z) :- add(X,Y,Z).
 add(s(X),Y,s(Z)) :- add(X,Y,Z).
 add(p(X),Y,p(Z)) :- add(X,Y,Z).
 
-% non minus sign
+add2(-X,Y,Z) :- minus(X,A),add2(A,Y,Z).
+add2(X,-Y,Z) :- minus(Y,A),add2(X,A,Z).
 add2(0,X+Y,Z) :- expand(X,0,A),expand(Y,0,B),add2(A,B,Z).
 add2(X+Y,0,Z) :- expand(X,0,A),expand(Y,0,B),add2(A,B,Z).
-add2(X+Y,s(A+B),Z) :- add2(X,Y,W), add2(s(0),A+B,C),add2(W,C,Z).
-add2(s(X+Y),A+B,Z) :- add2(s(0),X+Y,W), add2(A,B,C),add2(W,C,Z).
-add2(X,s(A+B),Z) :- add2(s(0),A+B,C),add2(X,C,Z).
-add2(s(X+Y),A,Z) :- add2(s(0),X+Y,W),add2(W,A,Z).
-add2(s(X+Y),s(A+B),Z) :- add2(s(0),X+Y,W),add2(s(0),A+B,C),add2(W,C,Z).
-add2(X+Y,p(A+B),Z) :- add2(X,Y,W), add2(p(0),A+B,C),add2(W,C,Z).
-add2(p(X+Y),A+B,Z) :- add2(p(0),X+Y,W), add2(A,B,C),add2(W,C,Z).
-add2(X,p(A+B),Z) :- add2(p(0),A+B,C),add2(X,C,Z).
-add2(p(X+Y),A,Z) :- add2(p(0),X+Y,W),add2(W,A,Z).
-add2(p(X+Y),p(A+B),Z) :- add2(p(0),X+Y,W),add2(p(0),A+B,C),add2(W,C,Z).
+add2(X+Y,s(A+B),Z) :- expand(X,0,C),expand(Y,0,D),expand(A,0,E),expand(B,0,F),add2(C,D,G),add2(s(0),E+F,H),add2(G,H,Z).
+add2(s(X+Y),A+B,Z) :-  expand(X,0,C),expand(Y,0,D),expand(A,0,E),expand(B,0,F),add2(E,F,G),add2(s(0),C+D,H),add2(G,H,Z).
+add2(X,s(A+B),Z) :- expand(X,0,C),expand(A,0,D),expand(B,0,E),add2(s(0),D+E,F),add2(C,F,Z).
+add2(s(X+Y),A,Z) :- expand(X,0,C),expand(Y,0,D),expand(A,0,E),add2(s(0),C+D,F),add2(F,E,Z).
+add2(s(X+Y),s(A+B),Z) :- expand(X,0,C),expand(Y,0,D),expand(A,0,E),expand(B,0,F),add2(s(0),E+F,G),add2(s(0),C+D,H),add2(G,H,Z).
+
+add2(X+Y,p(A+B),Z) :- expand(X,0,C),expand(Y,0,D),expand(A,0,E),expand(B,0,F),add2(C,D,G),add2(p(0),E+F,H),add2(G,H,Z).
+add2(p(X+Y),A+B,Z) :-  expand(X,0,C),expand(Y,0,D),expand(A,0,E),expand(B,0,F),add2(E,F,G),add2(p(0),C+D,H),add2(G,H,Z).
+add2(X,p(A+B),Z) :- expand(X,0,C),expand(A,0,D),expand(B,0,E),add2(p(0),D+E,F),add2(C,F,Z).
+add2(p(X+Y),A,Z) :- expand(X,0,C),expand(Y,0,D),expand(A,0,E),add2(p(0),C+D,F),add2(F,E,Z).
+add2(p(X+Y),p(A+B),Z) :- expand(X,0,C),expand(Y,0,D),expand(A,0,E),expand(B,0,F),add2(p(0),E+F,G),add2(p(0),C+D,H),add2(G,H,Z).
 add2(X+Y,R,Z) :- expand(X,0,A),expand(Y,0,B),expand(R,0,C),add2(A,B,D),add2(D,C,Z).
 add2(X,Y+R,Z) :- expand(X,0,A),expand(Y,0,B),expand(R,0,C),add2(A,B,D),add2(D,C,Z).
 add2(X+Y,A+B,Z) :- expand(X,0,D),expand(Y,0,E),expand(A,0,F),expand(B,0,G),add2(D,E,H),add2(F,G,I),add2(H,I,Z).
-
+ 
 
 %minus sign
+/*
 add2(0,(-X)+Y,Z) :- minus(X,A),expand(A,0,B),expand(Y,0,C),add2(B,C,Z).
 add2(0,X+(-Y),Z) :- minus(Y,A),expand(A,0,B),expand(X,0,C),add2(B,C,Z).
 add2((-X)+Y,0,Z) :- minus(X,A),expand(A,0,B),expand(Y,0,C),add2(B,C,Z).
@@ -57,11 +62,14 @@ add2(X+Y,p(A+(-B)),Z):-add2(X,Y,W),minus(B,D),add2(A,D,C),add2(p(0),C,E),add2(W,
 add2(p((-X)+Y),A+B,Z) :- minus(X,D),add2(D,Y,C),add2(p(0),C,E),add2(A,B,F),add2(E,F,Z).
 add2(p(X+(-Y)),A+B,Z) :- minus(Y,D),add2(X,D,C),add2(p(0),C,E),add2(A,B,F),add2(E,F,Z).
 add2((-X),p(A+B),Z) :- minus(X,D),add2(p(0),A+B,C),add2(D,C,Z).
-add2(X,p((-A)+B),Z) :- minus(A,D),add2(D,B,C),add2(p(0),C,E),add2(X,E,Z).
+add2(X,p((-A)+B),Z) :- minus(A,D),add2(D,B,C),add2(p(0),C,E),add2(X,E,Z).minus(s(0+p(0+s(0)))+s(0)+s(0)+s(0+p(0+s(0))),Z).
 add2(X,p(A+(-B)),Z) :- minus(B,D),add2(A,D,C),add2(p(0),C,E),add2(X,E,Z).
-add2(p((-X)+Y),A,Z) :- minus(X,D),add2(D,Y,C),add2(p(0),C,E),add2(E,A,Z).
-add2(p(X+(-Y)),A,Z) :- minus(Y,D),add2(X,D,C),add2(p(0),C,E),add2(E,A,Z).
-add2(p((-X)+Y),p(A+B),Z) :- minus(X,D),add2(p(0),D+Y,E),add2(p(0),A+B,F),add2(E,F,Z).
+add2(p((-X)+Y),A,Z) :- minus(X,D),add2(D,Y,C),add2add(s(X),p(Y),Z) :- add(X,Y,Z).
+add(p(X),s(Y),Z) :- add(X,Y,Z).(p(0),C,E),add2(E,A,Z).
+add2(p(X+(-Y)),A,Z) :- minus(Y,D),add2(X,D,C),add2add(s(X),p(Y),Z) :- add(X,Y,Z).
+add(p(X),s(Y),Z) :- add(X,Y,Z).(p(0),C,E),add2(E,A,Z).
+add2(p((-X)+Y),p(A+B),Z) :- minus(X,D),add2(p(0),Dadd(s(X),p(Y),Z) :- add(X,Y,Z).
+add(p(X),s(Y),Z) :- add(X,Y,Z).+Y,E),add2(p(0),A+B,F),add2(E,F,Z).
 add2(p(X+(-Y)),p(A+B),Z) :- minus(Y,D),add2(p(0),X+D,E),add2(p(0),A+B,F),add2(E,F,Z).
 add2(p(X+Y),p((-A)+B),Z) :- minus(A,D),add2(p(0),X+Y,E),add2(p(0),D+B,F),add2(E,F,Z).
 add2(p(X+Y),p(A+(-B)),Z) :- minus(B,D),add2(p(0),X+Y,E),add2(p(0),A+D,F),add2(E,F,Z).
@@ -79,7 +87,7 @@ add2((-X)+Y,A+B,Z) :- minus(X,W),expand(Y,0,E),expand(A,0,F),expand(B,0,G),add2(
 add2(X+(-Y),A+B,Z) :- minus(Y,W),expand(X,0,E),expand(A,0,F),expand(B,0,G),add2(W,E,H),add2(F,G,I),add2(H,I,Z).
 add2(X+Y,(-A)+B,Z) :- minus(A,W),expand(X,0,C),expand(Y,0,E),expand(B,0,G),add2(C,E,H),add2(W,G,I),add2(H,I,Z).
 add2(X+Y,A+(-B),Z) :- minus(B,W),expand(X,0,C),expand(Y,0,E),expand(A,0,G),add2(C,E,H),add2(W,G,I),add2(H,I,Z).
-
+*/
 add2(X,Y,Z) :- add(X,Y,Z).
 
 /*
@@ -116,5 +124,9 @@ Exercise 2
 Exercise 3
   test case 
     1.minus(s(0+p(0+s(0)))+s(0)+s(0)+s(0+p(0+s(0))),Z).
+Exercise 4
+  test case
+    1.add2(-s(0+s(0+s(0)))+s(0)+s(0),s(0+s(0+s(0))),Z).
+    2.add2(-s(0+p(0+s(0)))+s(0)+s(0),-s(0+p(0+s(0))),Z)
 */
 
